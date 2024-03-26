b4ca9444424c6f821eba079b348ad831db93d4c1
Justin <34963248+mej504@users.noreply.github.com>
2024-03-26 11:42:01 -0400

    pull out shortcode panel builder into private method; cleanup

--- src/NovaTinymceField.php
+++ src/NovaTinymceField.php
@@ -18,8 +18,16 @@ class NovaTinymceField extends Field implements StorableContract
     {
         parent::__construct($name, $attribute);
 
+        $options = config('nova.nova-tinymce-field.options');
+
+        // If we have shortcodes in the config, build out the panels for our
+        // custom shortcodes plugin
+        if (!empty($options['shortcodes'])) {
+            $options['shortcodes'] = $this->buildShortcodePanels($options['shortcodes']);
+        }
+
         $this->withMeta([
-            'options' => config('nova.nova-tinymce-field.options'),
+            'options' => $options,
         ]);
     }
 
@@ -30,5 +38,24 @@ class NovaTinymceField extends Field implements StorableContract
         $this->disk($disk)->path($path);
 
         return $this;
+    }
+
+    /**
+     * Takes an array of shortcode classes and converts them to individual
+     * panels to be consumed by our custom TinyMCE shortcode plugin
+     *
+     * @param string[] $shortcodes - an array of fully-qualified class names
+     *
+     * @return array
+     */
+    private function buildShortcodePanels(array $shortcodes): array
+    {
+        $panels = array_map(
+            fn($shortcodeClass) => $shortcodeClass::getWysiwygPanels() ?? null,
+            $shortcodes
+        );
+
+        // return array with null values removed
+        return array_filter($panels);
     }
 }
